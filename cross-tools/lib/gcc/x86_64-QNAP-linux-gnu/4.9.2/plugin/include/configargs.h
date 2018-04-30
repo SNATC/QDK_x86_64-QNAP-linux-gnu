da0cfb48e2b2229230bc4dfbbcc19dd58c973a71
OpenSNA <opensna@gmail.com>
2018-04-30 20:23:40 +0800

    first commit

---
+++ cross-tools/lib/gcc/x86_64-QNAP-linux-gnu/4.9.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.9.2/configure --with-pkgversion='toolchain config: [gcc-4.9.2 binutils-2.25 glibc-2.21]' --prefix=/root/toolchain/docker_deploy/dockerfile/CT_auto_build/CT/x86_64-QNAP-linux-gnu/cross-tools --with-sysroot=/root/toolchain/docker_deploy/dockerfile/CT_auto_build/CT/x86_64-QNAP-linux-gnu/cross-tools/../fs --enable-tls --disable-nls --enable-languages=c,c++ --enable-c99 --enable-__cxa_atexit --enable-shared --enable-long-long --disable-multilib --enable-threads=posix --disable-libgomp --enable-checking=release --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=x86_64-QNAP-linux-gnu gcc_cv_libc_provides_ssp=yes";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
